e070d751191ccaba9f5d71c8795219d7bf2520a5
Olivier Lemasle <olivier.lemasle@apalia.net>
2015-12-13 22:24:50 +0100

    Fix unit test

--- ui/test/main.spec.js
+++ ui/test/main.spec.js
@@ -13,16 +13,16 @@ describe('Go-Top main page', function() {
   it('should have some menus', function() {
     var menus = element.all(by.repeater('menu in menus'));
     expect(menus.count()).toBeGreaterThan(1);
-    expect(menus.first().getText()).toEqual('About');
+    expect(menus.first().getText()).toEqual('CPU USAGE');
   });
 
   it('should navigate to the /About page when clicking on the first menu', function() {
     element.all(by.repeater('menu in menus')).first().element(by.tagName('a')).click();
-    expect(browser.getLocationAbsUrl()).toEqual('/about');
+    expect(browser.getLocationAbsUrl()).toEqual('/');
   });
 
   it('should stay on the home page when clicking on the last menu', function() {
     element.all(by.repeater('menu in menus')).last().element(by.tagName('a')).click();
-    expect(browser.getLocationAbsUrl()).toEqual('/');
+    expect(browser.getLocationAbsUrl()).toEqual('/help');
   });
 });
